0899a0bc151707abe5964a028a6a13cfd41c751f
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2021-07-28 10:19:43 +0200

    api tags

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,8 +2,10 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('users')
+@ApiTags('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
