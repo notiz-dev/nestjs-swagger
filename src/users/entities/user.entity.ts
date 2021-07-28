62ce314c7f40496bf2cc824e0bded6e98236b4cb
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2021-07-28 11:37:47 +0200

    add responses

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,9 @@
-export class User {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class User {
+  @ApiProperty()
+  email: string;
+  password: string;
+  @ApiProperty({ required: false })
+  name?: string;
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { User } from './entities/user.entity';
 
 @Controller('users')
 @ApiTags('users')
@@ -10,26 +19,31 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @ApiCreatedResponse({ type: User })
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
+  @ApiOkResponse({ type: [User] })
   findAll() {
     return this.usersService.findAll();
   }
 
   @Get(':id')
+  @ApiOkResponse({ type: User })
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiCreatedResponse({ type: User })
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
   }
 
   @Delete(':id')
+  @ApiOkResponse({ type: User })
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id);
   }
