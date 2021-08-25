203a6c5135a9f74d2a21fdbe414d19e27a79aa70
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2021-08-25 17:15:51 +0200

    add image and pdf file decorator

--- src/files/api-file.decorator.ts
+++ src/files/api-file.decorator.ts
@@ -2,6 +2,7 @@ import { applyDecorators, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { MulterOptions } from '@nestjs/platform-express/multer/interfaces/multer-options.interface';
 import { ApiBody, ApiConsumes } from '@nestjs/swagger';
+import { fileMimetypeFilter } from './file-mimetype-filter';
 
 export function ApiFile(
   fieldName: string = 'file',
@@ -24,4 +25,22 @@ export function ApiFile(
       },
     }),
   );
+}
+
+export function ApiImageFile(
+  fileName: string = 'image',
+  required: boolean = false,
+) {
+  return ApiFile(fileName, required, {
+    fileFilter: fileMimetypeFilter('image'),
+  });
+}
+
+export function ApiPdfFile(
+  fileName: string = 'image',
+  required: boolean = false,
+) {
+  return ApiFile(fileName, required, {
+    fileFilter: fileMimetypeFilter('pdf'),
+  });
 }
