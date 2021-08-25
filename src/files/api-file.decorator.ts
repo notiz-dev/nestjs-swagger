22f340a567d39acd36938c38a7c54961c11a3d1a
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2021-08-25 13:56:45 +0200

    add multeroptions parameter

--- src/files/api-file.decorator.ts
+++ src/files/api-file.decorator.ts
@@ -1,10 +1,14 @@
 import { applyDecorators, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { MulterOptions } from '@nestjs/platform-express/multer/interfaces/multer-options.interface';
 import { ApiBody, ApiConsumes } from '@nestjs/swagger';
 
-export function ApiFile(fieldName: string = 'files') {
+export function ApiFile(
+  fieldName: string = 'files',
+  localOptions?: MulterOptions,
+) {
   return applyDecorators(
-    UseInterceptors(FileInterceptor(fieldName)),
+    UseInterceptors(FileInterceptor(fieldName, localOptions)),
     ApiConsumes('multipart/form-data'),
     ApiBody({
       schema: {
