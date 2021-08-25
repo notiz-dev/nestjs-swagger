4002aa6deaa2d9c696537553657f4e94a0bbcd66
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2021-08-25 12:13:41 +0200

    add multer types and single file upload

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { FilesModule } from './files/files.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, FilesModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/files/files.controller.ts
@@ -0,0 +1,21 @@
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { ApiTags } from '@nestjs/swagger';
+import { FilesService } from './files.service';
+
+@Controller('files')
+@ApiTags('files')
+export class FilesController {
+  constructor(private readonly filesService: FilesService) {}
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log(file);
+  }
+}
